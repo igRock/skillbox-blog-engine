421c7b759865bf793342555448271dcfb4305cf0
igRock <topchiy.greg@yandex.ru>
2020-01-23 22:32:19 +0300

    step 1 done

---
+++ src/main/java/ru/skillbox/blog_engine/controller/DefaultController.java
@@ -0,0 +1,12 @@
+package ru.skillbox.blog_engine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
