e76cafe98309ec997bafa0691baa102f8a13d5be
igRock <topchiy.greg@yandex.ru>
2020-04-07 22:28:38 +0300

    registry added

--- src/main/java/ru/skillbox/blog_engine/config/WebConfig.java
+++ src/main/java/ru/skillbox/blog_engine/config/WebConfig.java
@@ -1,7 +1,9 @@
 package ru.skillbox.blog_engine.config;
 
+import java.io.File;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.skillbox.blog_engine.enums.*;
 
@@ -15,5 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addConverter(new Decision.StringToEnumConverter());
         registry.addConverter(new Vote.StringToEnumConverter());
         registry.addConverter(new StatisticsType.StringToEnumConverter());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        final String rootPath = new File("").getAbsolutePath()
+            .concat("/src/main/resources");
+        final String uploadPath = "/static/img/upload";
+
+        registry.addResourceHandler(String.format("%s/*", uploadPath))
+            .addResourceLocations(String.format("file://%s", rootPath.concat(uploadPath)));
     }
 }
